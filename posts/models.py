9e51ded979bfec4af82c7c591596e583523d674e
Nacef LABIDI <nacef.labidi@gmail.com>
2016-11-24 10:45:35 +0100

    Initial commit

---
+++ posts/models.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Post(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
+
+
+class Theme(models.Model):
+    pass
